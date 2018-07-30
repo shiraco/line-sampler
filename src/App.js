87595e9e295c8014bb0635a3a24f3b7be99e6dce
Koji Shiraishi <shiraco@gmail.com>
2018-07-30 13:01:48 +0900

    create line-scheme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      line_id: 'xhc0394t',
+      line_text: 'hello',
+    }
+  }
+
+  handleLineSchemeLineIdChange(line_id) {
+    this.setState({ line_id });
+  }
+
+  handleLineSchemeLineTextChange(line_text) {
+    this.setState({ line_text });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +27,32 @@ class App extends Component {
         </header>
 
         <p className="App-intro">
-          <h2>Hello, world!</h2>
+          <h2>line-scheme</h2>
+
+          LINE ID:
+          <input
+            type="text"
+            value={this.state.line_id}
+            onChange={e => this.handleLineSchemeLineIdChange(e.target.value)}
+          />
+
+          text:
+          <input
+            type="text"
+            value={this.state.line_text}
+            onChange={e => this.handleLineSchemeLineTextChange(e.target.value)}
+          />
+
+          <p>
+            <a href="https://line.me/R/oaMessage/@{this.state.line_id}/?{this.state.line_text}">https://line.me/R/oaMessage/@{this.state.line_id}/?{this.state.line_text}</a>
+          </p>
+
+          <p>
+            <a href="line://oaMessage/@{this.state.line_id}/?{this.state.line_text}">line://oaMessage/@{this.state.line_id}/?{this.state.line_text}</a>
+          </p>
+
         </p>
+
 
       </div>
     );
